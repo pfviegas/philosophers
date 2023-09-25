640a96e4dfb625c6fd37c13057de9eca45c2e190
Paulo Viegas <pviegas@c2r10s4.42lisboa.com>
2023-09-25 17:00:10 +0100

    v1.9

---
+++ philo/src/checks.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checks.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pviegas <pviegas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 13:05:16 by pviegas           #+#    #+#             */
+/*   Updated: 2023/09/20 14:09:16 by pviegas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosophers.h"
+
+int	check_argc(int argc)
+{
+	if (argc < 5 || argc > 6)
+	{
+		printf("Sixtax Error: Wrong number of arguments\n");
+		printf("Arguments:\n\t./philo\n\tnumber_of_philosophers\n\t");
+		printf("time_to_die\n\ttime_to_eat\n\ttime_to_sleep\n\t");
+		printf("[number_of_times_each_philosopher_must_eat]\n");
+		return (1);
+	}
+	return (0);
+}
+
+int	check_argv(char *argv[])
+{
+	int	num_philos;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	num_meals;
+
+	num_philos = ft_atoi(argv[1]);
+	time_to_die = ft_atoi(argv[2]);
+	time_to_eat = ft_atoi(argv[3]);
+	time_to_sleep = ft_atoi(argv[4]);
+	if (argv[5])
+		num_meals = ft_atoi(argv[5]);
+	else
+		num_meals = 0;
+	if (num_philos < 1 || num_philos > 200 || time_to_die < 0
+		|| time_to_eat < 0 || time_to_sleep < 0
+		|| num_meals < 0)
+	{
+		printf("Error: Invalid arguments\n");
+		return (1);
+	}
+	return (0);
+}
